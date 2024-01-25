6ede594591ece7b3bbe76dd3516ec7391e55b577
John Marc Dela Cruz <jmarcdcruz@gmail.com>
2024-01-24 21:34:05 -0500

    committing 8kyu folder for organization

---
+++ 8kyu/01.11.2024-welcomeToTheCity.js
@@ -0,0 +1,4 @@
+function sayHello( name, city, state ) {
+    let names = name.join(' ')
+    return `Hello, ${names}! Welcome to ${city}, ${state}!`
+  }
---
+++ 8kyu/01.23.24-surfaceAreaAndVolumeOfABox8kyu.js
@@ -0,0 +1,8 @@
+function getSize(width, height, depth) {
+  
+  let area = (2*width*height) + (2*width*depth) + (2*height*depth)
+  let volume = width * height * depth
+ 
+  return [area, volume]
+ 
+}
